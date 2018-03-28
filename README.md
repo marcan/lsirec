127b56fdecb80f15954d16e77f743b0f4cd2bb18
Hector Martin <marcan@marcan.st>
2018-03-29 06:19:59 +0900

    Add host boot support

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 ## lsirec - LSI SAS2008/2108 HBA low-level recovery tool for Linux
 
-Currently supports reading and writing the SBR. In the future it may support
-cold-booting the adapter to recover bricked adapters or crossflash between
-firmwares directly from Linux.
+Currently supports reading and writing the SBR and booting the card in host
+boot mode.
 
 Use [lsiutil](https://github.com/exactassembly/meta-xa-stm/blob/master/recipes-support/lsiutil/files/)
 to crossflash between IT/IR firmwares from Linux, without vendor/product ID
@@ -59,12 +58,144 @@ When the system comes back up, if all went well, launch `lsiutil -e` again and
 use `18.  Change SAS WWID` to update the WWID if necessary, then reboot again
 (this writes it to the config section in Flash, not to the SBR).
 
+*NEW*: instead of rebooting, you can use:
+
+`# ./lsirec 0000:01:00.0 reset`
+
+`# ./lsirec 0000:01:00.0 rescan`
+
+Make sure your disks are not in use if you do this. `reset` might fail if you
+have just flashed a new firmware. This is normal, as the adapter takes a while
+to copy the firmware to the backup area on first boot. Wait a few seconds and
+use `# ./lsirec 0000:01:00.0 info` until it returns `IOC is READY`.
+
+## UNTESTED procedure to convert from MegaRAID to IT/IR firmware or recover a bricked card
+
+WARNING: this is completely untested. Host boot support has only been tested
+so far on a card that was already IT/IR. Please report back if you try this.
+This process initially boots the IT/IR firwmare without touching Flash, and I'm
+not sure if it might balk at whatever MegaRAID stuff was left there before we
+have a chance to wipe it.
+
+This procedure (obviously) resets the adapter, so make sure your disks are not
+in use and any dm/md/lvm mappings have been removed!
+
+This mode requires HugeTLB support enabled in your kernel:
+
+`# echo 16 > /proc/sys/vm/nr_hugepages`
+
+This process is also incompatible with IOMMUs. If you have one, make sure it
+is not active (e.g. check that `/sys/kernel/iommu_groups` is an empty
+directory).
+
+Make note of your SAS WWID (e.g. using MegaCLI or the kernel interfaces).
+
+`# ./lsirec 0000:01:00.0 unbind`
+
+Where 0000:01:00.0 is your PCI device ID. Unbind the kernel driver (if any).
+
+`# ./lsirec 0000:01:00.0 readsbr sbr_backup.bin`
+
+Back up your MegaRAID SBR.
+
+`# python3 sbrtool.py parse sbr_backup.bin sbr.cfg`
+
+Edit sbr.cfg with your favorite text editor. You'll probably want to use an IT
+SBR as a template, such as
+[sbr_sas9211-8i_itir.cfg](sample_sbr/sbr_sas9211-8i_itir.cfg). At the very
+least you need to set `PCIPID` properly (`0x0072` for SAS2008-based cards) and
+set `Interface` to `0x00` for IT/IR mode. You may want to add
+`SASAddr = 0xYOUR_SAS_WWID` to make the SAS WWID persist in the SBR (I'm not
+sure which firmwares use this, but I've seen it in some SBRs).
+
+`# python3 sbrtool.py build sbr.cfg sbr_new.bin`
+
+`# ./lsirec 0000:01:00.0 writesbr sbr_new.bin`
+
+Write the new IT/IR mode SBR. This does not immediately take effect.
+
+`# ./lsirec 0000:01:00.0 hostboot 2118it.bin`
+
+Where 2118it.bin is your desired firmware. If all went well, you should see
+something like this:
+
+```
+# ./lsirec 0000:01:00.0 hostboot 2118it.bin
+Device in MPT mode
+Resetting adapter in HCB mode...
+Trying unlock in MPT mode...
+Device in MPT mode
+IOC is RESET
+Setting up HCB...
+HCDW virtual: 0x7fca79e00000
+HCDW physical: 0x439a00000
+Loading firmware...
+Loaded 722708 bytes
+Booting IOC...
+IOC is READY
+IOC Host Boot successful.
+```
+
+At this point the PCI VID/PID should've changed, but the kernel will not have
+noticed. Check with lspci:
+
+`# lspci -vns 0000:01:00.0 -A linux-sysfs | head -n 2`
+
+`# lspci -vns 0000:01:00.0 -A intel-conf1 | head -n 2`
+
+The first command should still show the old VID/PID, but the second one should
+show the new (MPT mode) ones. To make the kernel notice:
+
+`# ./lsirec 0000:01:00.0 rescan`
+
+This removes the PCI device from the kernel and requests a rescan. At this point
+the mpt3sas kernel driver should load. Check `dmesg` for any errors.
+
+If all went well, you can use lsiutil to wipe Flash and flash your new firmware:
+
+`# lsiutil -e`
+
+Select your adapter.
+
+`46.  Upload FLASH section` → `5.  Complete (all sections)`
+
+Make a complete Flash backup to be safe.
+
+`33.  Erase non-volatile adapter storage` → `3.  FLASH`, then also
+`8.  Persistent manufacturing config pages`
+
+Wipe the whole Flash. This will take a while. Option number 3 excludes the
+manufacturing config pages, so you need both.
+
+`2.  Download firmware (update the FLASH)`
+
+Flash the new firmware. Optionally, use
+`4.  Download/erase BIOS and/or FCode (update the FLASH)` to flash the BIOS/EFI
+module (not necessary if you're not booting from the adapter).
+
+Exit lsiutil.
+
+Finally, if all went well, reset into normal mode:
+
+`# ./lsirec 0000:01:00.0 reset`
+
+This might complain about IOC not becoming ready, but this is normal, as the
+first boot takes longer. Use `./lsirec 0000:01:00.0 info` after a few seconds
+and check for `IOC is READY`.
+
+`# ./lsirec 0000:01:00.0 rescan`
+
+If all went well, `dmesg` should show the driver loading again successfully.
+Launch `lsiutil -e` again and use `18.  Change SAS WWID` to update the WWID
+if necessary, then `reset` and `rescan` again to make sure the kernel sees the
+new WWID.
+
+Enjoy your shiny new IT/IR-mode HBA.
+
 ## Disclaimer
 
 This has barely been tested a couple of cards. Don't blame me if this bricks or
-smokes your HBA. For crossflashing MegaRAID cards to IT/IR, you'll still need
-to flash from DOS/UEFI for now, since lsiutil does not work with non-operational
-adapters under Linux.
+smokes your HBA.
 
 DO NOT attempt to use this tool on non-SAS2x08 chipsets. It probably won't work
 and may do horrible things. This tool deliberately does not check the VID/PID
@@ -74,7 +205,7 @@ try to write the SBR into any random PCI device too.
 I have tested this on an LSI SAS2108-based MegaRAID card (Fujitsu D2616) with
 MegaRAID firmware and it successfully backed up the SBR, but the action
 triggered a PCI error in syslog (though the controller kept working). Your
-mileage may vary.
+mileage may vary. I have not yet tried crossflashing it live to IT/IR mode.
 
 ## License
 
--- lsirec.c
+++ lsirec.c
@@ -4,15 +4,31 @@
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/errno.h>
+#include <sys/user.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #define MPI2_DOORBELL               0x00
 
+#define MPI2_DOORBELL_STATE_MASK    0xF0000000
+#define MPI2_DOORBELL_READY         0x10000000
+#define MPI2_DOORBELL_OPERATIONAL   0x20000000
+#define MPI2_DOORBELL_FAULT         0x40000000
+
 #define MPI2_WRSEQ                  0x04
 
 #define MPI2_DIAG                   0x08
+#define MPI2_DIAG_SBR_RELOAD        0x2000
+#define MPI2_DIAG_BOOTDEVICE_MASK   0x1800
+#define MPI2_DIAG_BOOTDEVICE_DEF    0x0000
+#define MPI2_DIAG_BOOTDEVICE_HCDW   0x0800
+#define MPI2_DIAG_CLR_FLASH_BAD_SIG 0x0400
+#define MPI2_DIAG_FORCE_HCB         0x0200
+#define MPI2_DIAG_HCB_MODE          0x0100
 #define MPI2_DIAG_WRITE_ENABLE      0x0080
+#define MPI2_DIAG_FLASH_BAD_SIG     0x0040
+#define MPI2_DIAG_RESET_HISTORY     0x0020
 #define MPI2_DIAG_RW_ENABLE         0x0010
 #define MPI2_DIAG_RESET_ADAPTER     0x0004
 #define MPI2_DIAG_HOLD_IOC_RESET    0x0002
@@ -23,6 +39,13 @@
 
 #define MPI2_DCR_DATA               0x38
 #define MPI2_DCR_ADDRESS            0x3c
+
+#define MPI2_HCDW_SIZE              0x74
+#define MPI2_HCDW_SIZE_SIZE_MASK    0xFFFFF000
+#define MPI2_HCDW_SIZE_HCB_ENABLE   0x00000001
+
+#define MPI2_HCDW_ADDR_LOW          0x78
+#define MPI2_HCDW_ADDR_HIGH         0x7C
 
 #define MR_DIAG_RW_DATA             0x24
 #define MR_DIAG_RW_ADDRESS_LOW      0x28
@@ -44,8 +67,14 @@
 #define EEPROM_TYPE_16BIT       0x01
 #define EEPROM_TYPE_8BIT        0x02
 
+#define HCDW_SIZE 0x200000
+
 typedef struct {
+    char pci_id[16];
+
     void *bar1;
+
+    void *hcdw;
 
     uint8_t sbr_addr;
     int eep_type;
@@ -103,32 +132,8 @@ static void lsi_unlock(lsi_dev_t *d)
     write32(d, d->r_wrseq, 0x0d);
 }
 
-static int lsi_open(const char *pci_id, lsi_dev_t *d)
+static int lsi_reopen(lsi_dev_t *d)
 {
-    char path[128];
-
-    memset(d, 0, sizeof(*d));
-
-    if (strlen(pci_id) > 16)
-        return -1;
-
-    sprintf(path, "/sys/bus/pci/devices/%s/resource1", pci_id);
-
-    int fd = open(path, O_RDWR);
-    if (fd < 0) {
-        perror("open bar1");
-        return fd;
-    }
-
-    d->bar1 = mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-
-    if (d->bar1 == MAP_FAILED) {
-        perror("mmap bar1");
-        return -1;
-    }
-
-    close(fd);
-
     uint32_t val;
 
     d->r_diag = MPI2_DIAG;
@@ -144,6 +149,9 @@ static int lsi_open(const char *pci_id, lsi_dev_t *d)
         return 0;
     }
 
+    if (read32(d, MR_DIAG) & MPI2_DIAG_WRITE_ENABLE)
+        goto megaraid;
+
     printf("Trying unlock in MPT mode...\n");
     lsi_unlock(d);
 
@@ -154,6 +162,7 @@ static int lsi_open(const char *pci_id, lsi_dev_t *d)
         return 0;
     }
 
+megaraid:
     d->r_diag = MR_DIAG;
     d->r_wrseq = MR_WRSEQ;
     d->r_rw_addr_high = MR_DIAG_RW_ADDRESS_HIGH;
@@ -178,6 +187,204 @@ static int lsi_open(const char *pci_id, lsi_dev_t *d)
     }
 
     fprintf(stderr, "Failed to unlock device\n");
+
+    return -1;
+}
+
+static int lsi_open(const char *pci_id, lsi_dev_t *d)
+{
+    char path[128];
+
+    memset(d, 0, sizeof(*d));
+
+    if (strlen(pci_id) > 15)
+        return -1;
+
+    strcpy(d->pci_id, pci_id);
+
+    sprintf(path, "/sys/bus/pci/devices/%s/resource1", pci_id);
+
+    int fd = open(path, O_RDWR);
+    if (fd < 0) {
+        perror("open bar1");
+        return fd;
+    }
+
+    d->bar1 = mmap(NULL, 0x1000, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+    if (d->bar1 == MAP_FAILED) {
+        perror("mmap bar1");
+        return -1;
+    }
+
+    close(fd);
+
+    return lsi_reopen(d);
+}
+
+static int lsi_setup_hcdw(lsi_dev_t *d)
+{
+    char path[128];
+    int fd;
+
+    printf("Setting up HCB...\n");
+
+    // Enable bus mastering
+    sprintf(path, "/sys/bus/pci/devices/%s/config", d->pci_id);
+
+    fd = open(path, O_RDWR);
+    if (fd < 0) {
+        perror("open config");
+        return fd;
+    }
+    if (lseek(fd, 4, SEEK_SET) < 0) {
+        perror("lseek cmd");
+        return -1;
+    }
+    uint16_t cmd;
+    if (read(fd, &cmd, 2) != 2) {
+        perror("read cmd");
+        return -1;
+    }
+    cmd |= 0x4; // bus master
+    if (lseek(fd, 4, SEEK_SET) < 0) {
+        perror("lseek cmd");
+        return -1;
+    }
+    if (write(fd, &cmd, 2) != 2) {
+        perror("write cmd");
+        return -1;
+    }
+    close(fd);
+
+    d->hcdw = mmap(NULL, HCDW_SIZE, PROT_READ|PROT_WRITE,
+                   MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB|MAP_LOCKED, 0, 0);
+
+    if (d->hcdw == MAP_FAILED) {
+        perror("mmap hcdw");
+        fprintf(stderr, "Do you have hugepages enabled?\n");
+        fprintf(stderr, "Try: echo 16 > /proc/sys/vm/nr_hugepages\n");
+        return -1;
+    }
+
+    printf("HCDW virtual: %p\n", d->hcdw);
+
+    fd = open("/proc/self/pagemap", O_RDONLY);
+    if (fd < 0) {
+        perror("open /proc/self/pagemap");
+        return fd;
+    }
+    if (lseek(fd, ((uintptr_t)d->hcdw) >> (PAGE_SHIFT - 3), SEEK_SET) < 0) {
+        perror("lseek /proc/self/pagemap");
+        return -1;
+    }
+    uint64_t phys;
+    if (read(fd, &phys, 8) != 8) {
+        perror("read /proc/self/pagemap");
+        return -1;
+    }
+    close(fd);
+
+    phys = (phys & ((1ULL<<55) - 1)) << PAGE_SHIFT;
+
+    printf("HCDW physical: 0x%lx\n", phys);
+
+    write32(d, MPI2_HCDW_ADDR_LOW, phys & 0xffffffff);
+    write32(d, MPI2_HCDW_ADDR_HIGH, phys >> 32);
+    write32(d, MPI2_HCDW_SIZE, (0xfffff000 & ~(HCDW_SIZE-1)) | 1);
+
+    return 0;
+}
+
+static int lsi_disable_hcdw(lsi_dev_t *d)
+{
+    write32(d, MPI2_HCDW_SIZE, 0);
+    write32(d, MPI2_HCDW_ADDR_LOW, 0);
+    write32(d, MPI2_HCDW_ADDR_HIGH, 0);
+
+    int ret = munmap(d->hcdw, HCDW_SIZE);
+    if (ret < 0) {
+        perror("munmap hcdw");
+        return ret;
+    }
+
+    return 0;
+}
+
+static int lsi_unbind_driver(lsi_dev_t *d)
+{
+    char path[128];
+    int ret;
+
+    sprintf(path, "/sys/bus/pci/devices/%s/driver/unbind", d->pci_id);
+
+    int fd = open(path, O_WRONLY|O_TRUNC);
+    if (fd < 0) {
+        if (errno == ENOENT)
+            return 0;
+        perror("open unbind");
+        return fd;
+    }
+
+    ret = write(fd, d->pci_id, strlen(d->pci_id));
+    if (ret != strlen(d->pci_id)) {
+        perror("write unbind");
+        close(fd);
+        return ret;
+    }
+
+    printf("Kernel driver unbound from device\n");
+
+    close(fd);
+    return 1;
+}
+
+static int lsi_rescan(lsi_dev_t *d)
+{
+    char path[128];
+    int ret;
+
+    printf("Removing PCI device...\n");
+
+    sprintf(path, "/sys/bus/pci/devices/%s/remove", d->pci_id);
+
+    int fd = open(path, O_WRONLY|O_TRUNC);
+    if (fd < 0) {
+        if (errno == ENOENT)
+            return 0;
+        perror("open remove");
+        return fd;
+    }
+
+    ret = write(fd, "1", 1);
+    if (ret != 1) {
+        perror("write remove");
+        close(fd);
+        return ret;
+    }
+
+    close(fd);
+
+    printf("Rescanning PCI bus...\n");
+
+    fd = open("/sys/bus/pci/rescan", O_WRONLY|O_TRUNC);
+    if (fd < 0) {
+        if (errno == ENOENT)
+            return 0;
+        perror("open rescan");
+        return fd;
+    }
+
+    ret = write(fd, "1", 1);
+    if (ret != 1) {
+        perror("write rescan");
+        close(fd);
+        return ret;
+    }
+
+    close(fd);
+
+    printf("PCI bus rescan complete.\n");
 
     return 0;
 }
@@ -325,6 +532,22 @@ static int lsi_i2c_init(lsi_dev_t *d)
     return 0;
 }
 
+static int lsi_i2c_close(lsi_dev_t *d)
+{
+    uint32_t val;
+
+    chip_write32(d, CHIP_I2C_RESET, 1);
+    do {
+        i2c_delay(d);
+    } while (chip_read32(d, CHIP_I2C_RESET) & 1);
+
+    val = dcr_read32(d, DCR_I2C_SELECT);
+    val &= ~0x800000;
+    dcr_write32(d, DCR_I2C_SELECT, val);
+
+    return 0;
+}
+
 static int lsi_i2c_read_sbr(lsi_dev_t *d, int offset, int len, uint8_t *buf)
 {
     i2c_start(d);
@@ -405,17 +628,42 @@ static int lsi_i2c_write_sbr(lsi_dev_t *d, int offset, int len, uint8_t *buf)
     return 0;
 }
 
-static int do_info(lsi_dev_t *d) {
+static void print_ioc_state(lsi_dev_t *d)
+{
+    uint32_t doorbell = read32(d, MPI2_DOORBELL);
+
+    printf("IOC is ");
+    if (!(doorbell & MPI2_DOORBELL_STATE_MASK)) {
+        printf("RESET ");
+    }
+    if (doorbell & MPI2_DOORBELL_READY) {
+        printf("READY ");
+    }
+    if (doorbell & MPI2_DOORBELL_OPERATIONAL) {
+        printf("OPERATIONAL ");
+    }
+    if (doorbell & MPI2_DOORBELL_FAULT) {
+        printf("FAULT ");
+    }
+    printf("\n");
+}
+
+static int do_info(lsi_dev_t *d)
+{
     printf("Registers:\n");
     printf(" DOORBELL:       0x%08x\n", read32(d, MPI2_DOORBELL));
     printf(" DIAG:           0x%08x\n", read32(d, d->r_diag));
     printf(" DCR_I2C_SELECT: 0x%08x\n", dcr_read32(d, DCR_I2C_SELECT));
     printf(" DCR_SBR_SELECT: 0x%08x\n", dcr_read32(d, DCR_SBR_CONFIG));
     printf(" CHIP_I2C_PINS:  0x%08x\n", chip_read32(d, CHIP_I2C_PINS));
+
+    print_ioc_state(d);
+
     return 0;
 }
 
-static int do_readsbr(lsi_dev_t *d, const char *filename) {
+static int do_readsbr(lsi_dev_t *d, const char *filename)
+{
     uint8_t sbr[256];
     int ret;
 
@@ -436,10 +684,13 @@ static int do_readsbr(lsi_dev_t *d, const char *filename) {
     }
     close(fd);
     printf("SBR saved to %s\n", filename);
+
+    lsi_i2c_close(d);
     return 0;
 }
 
-static int do_writesbr(lsi_dev_t *d, const char *filename) {
+static int do_writesbr(lsi_dev_t *d, const char *filename)
+{
     uint8_t sbr[256];
     int ret;
 
@@ -461,7 +712,167 @@ static int do_writesbr(lsi_dev_t *d, const char *filename) {
         return ret;
     printf("SBR written from %s\n", filename);
 
+    lsi_i2c_close(d);
     return 0;
+}
+
+static int do_reset(lsi_dev_t *d)
+{
+    int ret;
+    uint32_t val;
+
+    ret = lsi_unbind_driver(d);
+    if (ret < 0)
+        return ret;
+
+    printf("Resetting adapter...\n");
+    val = read32(d, d->r_diag);
+    val &= ~MPI2_DIAG_BOOTDEVICE_MASK;
+    val &= ~MPI2_DIAG_FORCE_HCB;
+    val &= ~MPI2_DIAG_HCB_MODE;
+    write32(d, d->r_diag, val);
+
+    usleep(100000);
+
+    val = read32(d, d->r_diag);
+    val |= MPI2_DIAG_RESET_ADAPTER;
+    write32(d, d->r_diag, val);
+
+    usleep(100000);
+    print_ioc_state(d);
+
+    for (int i = 0; i < 200; i++) {
+        if (read32(d, MPI2_DOORBELL) & MPI2_DOORBELL_READY)
+            break;
+        usleep(10000);
+    }
+
+    print_ioc_state(d);
+
+    if (!(read32(d, MPI2_DOORBELL) & MPI2_DOORBELL_READY)) {
+        printf("IOC failed to become ready\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int do_halt(lsi_dev_t *d)
+{
+    int ret;
+    uint32_t val;
+
+    ret = lsi_unbind_driver(d);
+    if (ret < 0)
+        return ret;
+
+    printf("Resetting adapter in HCB mode...\n");
+    val = read32(d, d->r_diag);
+    val |= MPI2_DIAG_FORCE_HCB;
+    write32(d, d->r_diag, val);
+    val |= MPI2_DIAG_RESET_ADAPTER;
+    write32(d, d->r_diag, val);
+
+    usleep(1000000);
+
+    lsi_reopen(d);
+
+    val = read32(d, d->r_diag);
+    val &= ~MPI2_DIAG_FORCE_HCB;
+    write32(d, d->r_diag, val);
+
+    print_ioc_state(d);
+
+    if (read32(d, MPI2_DOORBELL) & MPI2_DOORBELL_STATE_MASK) {
+        printf("IOC failed to stay in reset\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int do_hostboot(lsi_dev_t *d, const char *filename)
+{
+    int ret;
+    uint32_t val;
+
+    ret = do_halt(d);
+    if (ret < 0)
+        return ret;
+
+    val = read32(d, d->r_diag);
+    val |= MPI2_DIAG_CLR_FLASH_BAD_SIG;
+    val &= ~MPI2_DIAG_BOOTDEVICE_MASK;
+    val &= ~MPI2_DIAG_FORCE_HCB;
+    val &= ~MPI2_DIAG_RESET_HISTORY;
+    write32(d, d->r_diag, val);
+
+    ret = lsi_setup_hcdw(d);
+    if (ret < 0) {
+        return ret;
+    }
+
+    printf("Loading firmware...\n");
+    memset(d->hcdw, 0x42, HCDW_SIZE);
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        perror("open firmware");
+        return fd;
+    }
+    ssize_t length = read(fd, d->hcdw, HCDW_SIZE);
+    if (length < 0) {
+        perror("read firmware");
+        return length;
+    }
+    memmove(d->hcdw + HCDW_SIZE - length, d->hcdw, length);
+    printf("Loaded %ld bytes\n", length);
+
+    val = read32(d, d->r_diag);
+    val |= MPI2_DIAG_BOOTDEVICE_HCDW;
+    write32(d, d->r_diag, val);
+
+    printf("Booting IOC...\n");
+
+    val &= ~MPI2_DIAG_HOLD_IOC_RESET;
+    val &= ~MPI2_DIAG_FORCE_HCB;
+    write32(d, d->r_diag, val);
+
+    for (int i = 0; i < 200; i++) {
+        if (read32(d, MPI2_DOORBELL) & MPI2_DOORBELL_READY)
+            break;
+        usleep(10000);
+    }
+
+    print_ioc_state(d);
+
+    if (!(read32(d, MPI2_DOORBELL) & MPI2_DOORBELL_READY)) {
+        printf("IOC failed to become ready\n");
+        return -1;
+    }
+
+    usleep(500000);
+
+    printf("IOC Host Boot successful.\n");
+
+    lsi_disable_hcdw(d);
+
+    return 0;
+}
+
+static int do_unbind(lsi_dev_t *d)
+{
+    return lsi_unbind_driver(d);
+}
+
+static int do_rescan(lsi_dev_t *d)
+{
+    int ret;
+
+    ret = lsi_unbind_driver(d);
+    if (ret < 0)
+        return ret;
+
+    return lsi_rescan(d);
 }
 
 static void usage(char *argv0)
@@ -472,8 +883,29 @@ static void usage(char *argv0)
     fprintf(stderr, "\n");
     fprintf(stderr, "Supported operations:\n");
     fprintf(stderr, "  info\n");
+    fprintf(stderr, "    Print the device state and registers\n");
     fprintf(stderr, "  readsbr <sbr.bin>\n");
+    fprintf(stderr, "    Read the SBR.\n");
     fprintf(stderr, "  writesbr <sbr.bin>\n");
+    fprintf(stderr, "    Write the SBR.\n");
+    fprintf(stderr, " *reset\n");
+    fprintf(stderr, "    Perform a normal adapter reset. This also reloads\n");
+    fprintf(stderr, "    the SBR.\n");
+    fprintf(stderr, " *halt\n");
+    fprintf(stderr, "    Perform an HCB reset, not allowing the IOC to boot.\n");
+    fprintf(stderr, " *hostboot <firmware.bin>\n");
+    fprintf(stderr, "    Reset the adapter and boot the specified firmware\n");
+    fprintf(stderr, "    directly from host memory. Note: requires HugeTLB\n");
+    fprintf(stderr, "    and is not compatible with IOMMUs.\n");
+    fprintf(stderr, " *unbind\n");
+    fprintf(stderr, "    Unbind the kernel driver from the PCI device.\n");
+    fprintf(stderr, " *rescan\n");
+    fprintf(stderr, "    Tell the kernel to remove and rescan the PCI device.\n");
+    fprintf(stderr, "    This automatically picks up VID/PID changes and\n");
+    fprintf(stderr, "    rebinds the driver.\n");
+    fprintf(stderr, "\n");
+    fprintf(stderr, "* Operation forcefully unbinds the kernel driver. Make\n");
+    fprintf(stderr, "  sure your disks are not in use!\n");
     fprintf(stderr, "\n");
     fprintf(stderr, "Example: %s 0000:01:00.0 readsbr sbr.bin\n", argv0);
     fprintf(stderr, "\n");
@@ -497,6 +929,16 @@ int main(int argc, char **argv)
         return !!do_readsbr(&dev, argv[3]);
     } else if (!strcmp(argv[2], "writesbr") && argc == 4) {
         return !!do_writesbr(&dev, argv[3]);
+    } else if (!strcmp(argv[2], "reset")) {
+        return !!do_reset(&dev);
+    } else if (!strcmp(argv[2], "halt")) {
+        return !!do_halt(&dev);
+    } else if (!strcmp(argv[2], "hostboot") && argc == 4) {
+        return !!do_hostboot(&dev, argv[3]);
+    } else if (!strcmp(argv[2], "unbind")) {
+        return !!do_unbind(&dev);
+    } else if (!strcmp(argv[2], "rescan")) {
+        return !!do_rescan(&dev);
     } else {
         usage(argv[0]);
         return 1;
